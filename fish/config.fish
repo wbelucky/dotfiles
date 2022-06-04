9c33e7bd2078cfb5bd092a9dc1476f9506fc5664
wbelucky <39439193+WBelucky@users.noreply.github.com>
2022-06-04 20:41:23 +0900

    fix git_ps1 of fish to use emoji

--- fish/config.fish
+++ fish/config.fish
@@ -10,12 +10,12 @@ if status is-interactive
     set __fish_git_prompt_color_upstream_behind red
 
     # Status Chars
-    set __fish_git_prompt_char_dirtystate '⚡'
-    set __fish_git_prompt_char_stagedstate '→'
-    set __fish_git_prompt_char_untrackedfiles '☡'
-    set __fish_git_prompt_char_stashstate '↩'
-    set __fish_git_prompt_char_upstream_ahead '+'
-    set __fish_git_prompt_char_upstream_behind '-'
+    set __fish_git_prompt_char_dirtystate '🚧'
+    set __fish_git_prompt_char_stagedstate '🏁'
+    set __fish_git_prompt_char_untrackedfiles '✨'
+    set __fish_git_prompt_char_stashstate '📚'
+    set __fish_git_prompt_char_upstream_ahead '⏩'
+    set __fish_git_prompt_char_upstream_behind '⏪'
 end
 
 
