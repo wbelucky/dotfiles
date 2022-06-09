537f34e906820d6ce9d2eabc97a8182efde635cb
wbelucky <39439193+WBelucky@users.noreply.github.com>
2022-06-09 14:07:10 +0900

    fix fish_prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,10 @@
+set fish_greeting ""
+
+
 if status is-interactive
+
+    set -gx TERM xterm-256color
+
     # Commands to run in interactive sessions can go here
     # Fish git prompt
     set __fish_git_prompt_showdirtystate 'yes'
@@ -16,6 +22,24 @@ if status is-interactive
     set __fish_git_prompt_char_stashstate '📚'
     set __fish_git_prompt_char_upstream_ahead '⏩'
     set __fish_git_prompt_char_upstream_behind '⏪'
+
+    command -qv gh && eval (gh completion -s fish| source)
 end
 
+set -gx EDITOR nvim
+set -gx PATH bin $PATH
+set -gx PATH ~/.bin $PATH
+set -gx PATH ~/.local/bin $PATH
+set -gx GOPATH $HOME/go
+set -gx PATH $GOPATH/bin $PATH
+set -gx PATH node_modules/.bin $PATH
+
+abbr -a 'clip' 'xclip -selection c'
+abbr -a 'dc' 'docker-compose'
+abbr -a 'dlmv' 'mv (ls -td $HOME/Downloads/* | head -n 1) .'
+abbr -a 'g' 'git'
+abbr -a 'gcd' 'cd (ghq list -p | fzf)'
+abbr -a 'open' 'xdg-open'
+
+command -qv nvim && alias vim nvim
 
--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,13 +1,27 @@
-function fish_prompt
+function _prompt_user
     test $SSH_TTY
     and printf (set_color red)$USER(set_color brwhite)'@'(set_color yellow)(prompt_hostname)' '
+
     test $USER = 'root'
     and echo (set_color red)"#"
-
-    # Main
-    echo -n (set_color cyan)(prompt_pwd) (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯'(set_color normal)
-
-    # Git
-    set last_status $status
-    printf '%s ' (__fish_git_prompt)
 end
+
+function _prompt_dir
+
+  # Git
+  echo -n (set_color cyan)(prompt_pwd) (set_color normal)(__fish_vcs_prompt) 
+end
+
+function fish_prompt
+    set -l last_status $status
+
+    _prompt_user
+    _prompt_dir
+
+    if [ $last_status -gt 0 ]
+      echo -n (set_color red)" [$last_status]"
+    end
+
+    echo -n (set_color normal)'❯ '
+end
+
