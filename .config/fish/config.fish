67712e6b8e4873442b535740ca5537d0e9515b23
wbelucky <39439193+WBelucky@users.noreply.github.com>
2022-06-09 16:29:41 +0900

    fix fish_prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,23 +5,6 @@ if status is-interactive
 
     set -gx TERM xterm-256color
 
-    # Commands to run in interactive sessions can go here
-    # Fish git prompt
-    set __fish_git_prompt_showdirtystate 'yes'
-    set __fish_git_prompt_showstashstate 'yes'
-    set __fish_git_prompt_showuntrackedfiles 'yes'
-    set __fish_git_prompt_showupstream 'yes'
-    set __fish_git_prompt_color_branch yellow
-    set __fish_git_prompt_color_upstream_ahead green
-    set __fish_git_prompt_color_upstream_behind red
-
-    # Status Chars
-    set __fish_git_prompt_char_dirtystate '🚧'
-    set __fish_git_prompt_char_stagedstate '🏁'
-    set __fish_git_prompt_char_untrackedfiles '✨'
-    set __fish_git_prompt_char_stashstate '📚'
-    set __fish_git_prompt_char_upstream_ahead '⏩'
-    set __fish_git_prompt_char_upstream_behind '⏪'
 
     command -qv gh && eval (gh completion -s fish| source)
 end
--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,3 +1,30 @@
+
+# Commands to run in interactive sessions can go here
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '🚧'
+set __fish_git_prompt_char_stagedstate '🏁'
+set __fish_git_prompt_char_untrackedfiles '✨'
+set __fish_git_prompt_char_stashstate '📚'
+set __fish_git_prompt_char_upstream_ahead '⏩'
+set __fish_git_prompt_char_upstream_behind '⏪'
+
+
+function _segment
+  set_color -b $argv[1] $argv[2]
+  echo -n \ue0b0
+end
+
+
 function _prompt_user
     test $SSH_TTY
     and printf (set_color red)$USER(set_color brwhite)'@'(set_color yellow)(prompt_hostname)' '
@@ -7,9 +34,8 @@ function _prompt_user
 end
 
 function _prompt_dir
-
   # Git
-  echo -n (set_color cyan)(prompt_pwd) (set_color normal)(__fish_vcs_prompt) 
+  echo -n (set_color cyan)(prompt_pwd) (set_color normal)(__fish_git_prompt "(%s)") 
 end
 
 function fish_prompt
@@ -21,7 +47,6 @@ function fish_prompt
     if [ $last_status -gt 0 ]
       echo -n (set_color red)" [$last_status]"
     end
-
-    echo -n (set_color normal)'❯ '
+    echo -n '> '
 end
 
