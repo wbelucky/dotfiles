1c1bc33d5316d6e05bd812923701d7d7ab94babc
wbelucky <39439193+WBelucky@users.noreply.github.com>
2022-06-30 16:05:30 +0900

    add aqua.yml

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -34,8 +34,16 @@ abbr -a 'gcl' 'ghq get'
 abbr -a 'd' 'docker'
 abbr -a 'dps' 'docker ps'
 abbr -a 'di' 'docker images'
+abbr -a ls 'exa --icons --git'
 
 command -qv nvim && alias vim nvim
+
+if command -qv exa
+  alias ll 'exa -l --icons --git'
+  alias lla 'll -a'
+  alias lt 'exa -T -L 3 -a -I "node_modules|.git|.cache" --icons'
+  alias ltl 'exa -T -L 3 -a -I "node_modules|.git|.cache" -l --icons'
+end
 
 fish_vi_key_bindings
 
