16073bdf09e4176807681981b40727c527ec978d
wbelucky <39439193+WBelucky@users.noreply.github.com>
2022-07-01 11:34:38 +0900

    add env to config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,14 +8,19 @@ end
 
 set -gx EDITOR nvim
 set -gx GOPATH $HOME/go
+
+set -gx DOTFILES $HOME/ghq/github.com/wbelucky/dotfiles-with-docker
+set -gx AQUA_GLOBAL_CONFIG $DOTFILES/aqua.yaml
+
+set -q XDG_DATA_HOME || set -gx XDG_DATA_HOME $HOME/.local/share
+set -q AQUA_ROOT_DIR || set -gx AQUA_ROOT_DIR $XDG_DATA_HOME/aquaproj-aqua
+
 fish_add_path bin
 fish_add_path ~/.bin
 fish_add_path ~/.local/bin
 fish_add_path $GOPATH/bin
 fish_add_path node_modules/.bin
 
-set -q XDG_DATA_HOME || set -gx XDG_DATA_HOME $HOME/.local/share
-set -q AQUA_ROOT_DIR || set -gx AQUA_ROOT_DIR $XDG_DATA_HOME/aquaproj-aqua
 fish_add_path $AQUA_ROOT_DIR/bin
 
 abbr -a 'clip' 'xclip -selection c'
