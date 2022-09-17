6e8389446e6fa6357994157a282f88f1bfe1818c
wbelucky <39439193+WBelucky@users.noreply.github.com>
2022-09-17 18:26:19 +0900

    PlugInstall before makking symlink to .gitconfig

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 command -v aqua \
   || curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v1.0.0/aqua-installer | bash
 
-. ./scripts/link.sh
 . ./scripts/fish.sh
 aqua i
 . ./scripts/vim-plug.sh
+. ./scripts/link.sh
