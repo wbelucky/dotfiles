f36cdfc41db7da688f0a63fee58a2d8bd3d6f84d
wbelucky <39439193+WBelucky@users.noreply.github.com>
2022-09-20 19:39:12 +0900

    add executing cmd in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 USER=${USERNAME:-user}
-USER_ID=${LOCAL_UID:-9001}
-GROUP_ID=${LOCAL_GID:-9001}
+USER_ID=${LOCAL_UID:-1000}
+GROUP_ID=${LOCAL_GID:-1000}
 
 sudo usermod -u $USER_ID -o $USER
 sudo groupmod -g $GROUP_ID $USER
 
-# exec /usr/sbin/gosu $USER "$@"
+exec /usr/sbin/gosu $USER "$@"
