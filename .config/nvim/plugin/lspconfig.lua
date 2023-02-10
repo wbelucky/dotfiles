f24a9a1e72893614c23f85bfedbad5a17ab6e7b6
wbelucky <39439193+WBelucky@users.noreply.github.com>
2023-02-11 03:50:17 +0900

    sumneko_lua -> lua_ls

--- .config/nvim/plugin/lspconfig.lua
+++ .config/nvim/plugin/lspconfig.lua
@@ -139,7 +139,7 @@ nvim_lsp.tsserver.setup {
 }
 ]]
 
-nvim_lsp.sumneko_lua.setup {
+nvim_lsp.lua_ls.setup {
   capabilities = capabilities,
   on_attach = function(client, bufnr)
     on_attach(client, bufnr)
