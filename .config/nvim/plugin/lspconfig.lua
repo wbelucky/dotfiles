db05549da393c66567d619aed2275d419e12206f
wbelucky <39439193+WBelucky@users.noreply.github.com>
2023-02-26 20:59:23 +0900

    add mason.setup_handlers

--- .config/nvim/plugin/lspconfig.lua
+++ .config/nvim/plugin/lspconfig.lua
@@ -10,28 +10,11 @@ if (not status2) then return end
 local status, nvim_lsp = pcall(require, "lspconfig")
 if (not status) then return end
 
-local util = require('lspconfig/util')
 local protocol = require 'vim.lsp.protocol'
 
+local base = require('wbelucky.lsp_base')
+local servers = require('wbelucky.lsp_servers')
 
-mason.setup {}
-mason_lspconfig.setup {
-  automatic_installation = true,
-}
-
-local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
-local enable_format_on_save = function(_, bufnr)
-  -- if client.server_capabilities.documentFormattingProvider then
-  vim.api.nvim_clear_autocmds({ group = augroup_format, buffer = bufnr })
-  vim.api.nvim_create_autocmd("BufWritePre", {
-    group = augroup_format,
-    buffer = bufnr,
-    callback = function()
-      vim.lsp.buf.format({ bufnr = bufnr })
-    end,
-  })
-  -- end
-end
 
 local global_opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', global_opts)
@@ -40,34 +23,35 @@ vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', global_opts
 -- also use telescope.diagnostics
 vim.keymap.set('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', global_opts)
 
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(_, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+mason.setup {}
+mason_lspconfig.setup {
+  automatic_installation = true,
+}
 
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-  --Enable completion triggered by <c-x><c-o>
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  local opts = { noremap = true, silent = true }
-
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<Cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', 'gh', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<F2>', '<Cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', 'gn', '<Cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', '<leader>a', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  -- buf_set_keymap('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-
-
-end
-
+mason_lspconfig.setup_handlers({
+  function(server)
+    local default = {
+      capabilities = base.capabilities,
+      on_attach = base.on_attach,
+    }
+    nvim_lsp[server].setup(vim.tbl_extend("force", default, servers[server] or {}))
+  end,
+  ["tsserver"] = function()
+    require("typescript").setup({
+      disable_commands = false, -- prevent the plugin from creating Vim commands
+      debug = false, -- enable debug logging for commands
+      go_to_source_definition = {
+        fallback = true, -- fall back to standard LSP definition on failure
+      },
+      server = { -- pass options to lspconfig's setup method
+        on_attach = base.on_attach,
+        filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+        cmd = { "typescript-language-server", "--stdio" },
+        capabilities = base.capabilities
+      },
+    })
+  end,
+})
 
 --protocol.SymbolKind = { }
 protocol.CompletionItemKind = {
@@ -98,37 +82,6 @@ protocol.CompletionItemKind = {
   '', -- TypeParameter
 }
 
--- Set up completion using nvim_cmp with LSP source
-local capabilities = require('cmp_nvim_lsp').default_capabilities(
-  vim.lsp.protocol.make_client_capabilities()
-)
-
-nvim_lsp.pyright.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { "python" }
-}
-
-
-nvim_lsp.flow.setup {
-  on_attach = on_attach,
-  filetypes = { "javascript", "javascriptreact", "javascript.jsx" },
-  capabilities = capabilities
-}
-
-require("typescript").setup({
-  disable_commands = false, -- prevent the plugin from creating Vim commands
-  debug = false, -- enable debug logging for commands
-  go_to_source_definition = {
-    fallback = true, -- fall back to standard LSP definition on failure
-  },
-  server = { -- pass options to lspconfig's setup method
-    on_attach = on_attach,
-    filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-    cmd = { "typescript-language-server", "--stdio" },
-    capabilities = capabilities
-  },
-})
 
 --[[
 nvim_lsp.tsserver.setup {
@@ -139,27 +92,6 @@ nvim_lsp.tsserver.setup {
 }
 ]]
 
-nvim_lsp.lua_ls.setup {
-  capabilities = capabilities,
-  on_attach = function(client, bufnr)
-    on_attach(client, bufnr)
-    enable_format_on_save(client, bufnr)
-  end,
-  settings = {
-    Lua = {
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = { 'vim' },
-      },
-
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-        checkThirdParty = false
-      },
-    },
-  },
-}
 
 --[[ nvim_lsp.diagnosticls.setup {
   on_attach = on_attach,
@@ -221,61 +153,6 @@ nvim_lsp.lua_ls.setup {
   }
 }
 ]]
-
-nvim_lsp.gopls.setup {
-  on_attach = on_attach,
-  cmd = { "gopls", "serve" },
-  filetypes = { "go", "gomod" },
-  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-  settings = {
-    gopls = {
-      analyses = {
-        unusedparamas = true,
-      },
-      staticcheck = true,
-    }
-  }
-}
-
-nvim_lsp.clangd.setup {
-  on_attach = on_attach,
-  settings = {
-  }
-}
-
--- https://github.com/b0o/schemastore.nvim
-nvim_lsp.jsonls.setup {
-  on_attach = on_attach,
-  settings = {
-    json = {
-      schemas = require('schemastore').json.schemas(),
-      validate = { enable = true },
-    }
-  }
-}
-
--- https://www.reddit.com/r/neovim/comments/pta1ka/unable_to_configure_yamllanguageserver/
-nvim_lsp.yamlls.setup {
-  on_attach = on_attach,
-  settings = {
-    yaml = {
-      schemaStore = {
-        url = "https://www.schemastore.org/api/json/catalog.json",
-        enable = true,
-      },
-      hover = true,
-      completion = true,
-      validate = true,
-      format = {
-        enable = true
-      }
-    }
-  }
-}
-
-nvim_lsp.vimls.setup {
-  on_attach = on_attach
-}
 
 -- icon
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
