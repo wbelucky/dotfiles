a1980585160c70b6d218c537a395e44b7b5ed320
wbelucky <39439193+WBelucky@users.noreply.github.com>
2023-05-14 19:59:49 +0900

    ssh-agentの自動起動のためのkeychainの設定をconfig.fishに追加. 無駄なgh completionの読み込みを削除

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,5 @@
+echo "Loading .config/fish/config.fish ..."
 set fish_greeting ""
-
-if status is-interactive
-    set -gx TERM xterm-256color
-    command -qv gh && eval (gh completion -s fish| source)
-    source ~/.asdf/asdf.fish
-end
 
 set -gx EDITOR nvim
 set -gx GOPATH $HOME/go
@@ -36,49 +31,26 @@ while read -l var
   set -gx $var $val
 end
 
-abbr -a 'clip' 'xclip -selection c'
-abbr -a 'd' 'docker'
-abbr -a 'dc' 'docker-compose'
-abbr -a 'di' 'docker images'
-abbr -a 'dlmv' 'mv (ls -td $HOME/Downloads/* | head -n 1) .'
-abbr -a 'dps' 'docker ps'
-abbr -a 'g' 'git'
-abbr -a 'ga' 'git add'
-abbr -a 'gap' 'git add -p'
-abbr -a 'gc' 'git commit'
-abbr -a 'gcd' 'ghq_change_directory'
-abbr -a 'gf' 'git fetch'
-abbr -a 'gq' 'ghq_change_directory'
-abbr -a 'gcl' 'ghq get'
-abbr -a 'gd' 'git diff'
-abbr -a 'gdc' 'git diff --cached'
-abbr -a 'gl' 'git log'
-# ref: https://zenn.dev/takuya/articles/7550d21ddd17f121602e#%E3%83%AA%E3%83%A2%E3%83%BC%E3%83%88%E3%83%AA%E3%83%9D%E3%82%B8%E3%83%88%E3%83%AA%E3%81%ABpush%E3%81%99%E3%82%8B
-abbr -a 'gp' 'git push origin (git rev-parse --abbrev-ref HEAD)'
-abbr -a 'gpl' 'git pull origin (git rev-parse --abbrev-ref HEAD)'
-abbr -a 'grs' 'git reset'
-abbr -a 'gs' 'git status'
-abbr -a 'gsw' 'git switch'
-abbr -a 'gw' 'git_log_switch'
-
-if test -e /etc/os-release
-  abbr -a 'open' 'xdg-open'
-end
-
-command -qv nvim && alias vim nvim
-command -qv nvim && alias v nvim
-
-if command -qv exa
-  abbr -a ls 'exa --icons --git'
-  abbr -a ll 'exa -l --icons --git'
-  abbr -a lla 'll -a'
-  abbr -a lt 'exa -T -L 3 -a -I "node_modules|.git|.cache" --icons'
-  abbr -a ltl 'exa -T -L 3 -a -I "node_modules|.git|.cache" -l --icons'
-end
-
-fish_vi_key_bindings
 
 if set -q PRIVATE_CONFIGS
   source "$PRIVATE_CONFIGS/config-private.fish"
 end
 
+# for ssh-agent ref: https://zenn.dev/kaityo256/articles/ssh_agent_on_wsl
+if command -qv keychain
+  keychain -q --nogui $HOME/.ssh/id_ed25519
+  source $HOME/.keychain/$hostname-fish
+end
+
+if status is-interactive
+    source $HOME/.config/fish/abbr.fish
+
+    set -gx TERM xterm-256color
+    # ghにはデフォルトで保管の設定がなされている? https://github.com/fish-shell/fish-shell/blob/master/share/completions/gh.fish
+    # echo "Loading gh completion $(date "+%S%N")"
+    # command -qv gh && eval (gh completion -s fish | source)
+    # echo "Finish loading gh completion $(date "+%S%N")"
+    source ~/.asdf/asdf.fish
+end
+
+echo "Finish loading .config/fish/config.fish"
