c27c0470590530d1a7c703a94ef3fec8a064f5de
wbelucky <39439193+WBelucky@users.noreply.github.com>
2023-05-24 14:59:05 +0900

    fix mason-null-ls settings, and add path cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -2,25 +2,25 @@ local M = {
   "hrsh7th/nvim-cmp",
   module = { "cmp" },
   requires = {
-    { 'hrsh7th/cmp-nvim-lsp', event = { "InsertEnter" } },
+    { "hrsh7th/cmp-nvim-lsp", event = { "InsertEnter" } },
     { "hrsh7th/cmp-buffer", event = { "InsertEnter" } },
     { "hrsh7th/cmp-emoji", event = { "InsertEnter" } },
-    { 'hrsh7th/cmp-path', event = { "InsertEnter" } },
-    { 'hrsh7th/cmp-vsnip', event = { "InsertEnter" } },
-    { 'hrsh7th/cmp-cmdline', event = { "CmdlineEnter" } },
-    { 'hrsh7th/vim-vsnip', event = { "InsertEnter" } },
+    { "hrsh7th/cmp-path", event = { "InsertEnter" } },
+    { "hrsh7th/cmp-vsnip", event = { "InsertEnter" } },
+    { "hrsh7th/cmp-cmdline", event = { "CmdlineEnter" } },
+    { "hrsh7th/vim-vsnip", event = { "InsertEnter" } },
     -- ……以下各種ソースプラグインが続く
   },
 }
 -- set completeopt=menuone,noinsert,noselect
 
 M.config = function()
-  local cmp = require('cmp')
+  local cmp = require "cmp"
   if cmp == nil then
     return
   end
-  local lspkind = require 'lspkind'
-  cmp.setup({
+  local lspkind = require "lspkind"
+  cmp.setup {
     snippet = {
       expand = function(args)
         vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
@@ -29,55 +29,61 @@ M.config = function()
         -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
       end,
     },
-    mapping = cmp.mapping.preset.insert({
-      ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-d>'] = cmp.mapping.scroll_docs(4),
-      ['<C-j>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.close(),
-      ['<CR>'] = cmp.mapping.confirm({
+    mapping = cmp.mapping.preset.insert {
+      ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-d>"] = cmp.mapping.scroll_docs(4),
+      ["<C-j>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.close(),
+      ["<CR>"] = cmp.mapping.confirm {
         behavior = cmp.ConfirmBehavior.Replace,
-        select = true
-      }),
-    }),
+        select = true,
+      },
+    },
     sources = cmp.config.sources({
       {
-        name = 'vsnip',
-        keyword_length = 3
-      }, {
-        name = 'nvim_lsp',
-        keyword_length = 3
-      } -- { name = 'cmp_tabnine' },
+        name = "vsnip",
+        keyword_length = 3,
+      },
+      {
+        name = "nvim_lsp",
+        keyword_length = 3,
+      }, -- { name = 'cmp_tabnine' },
+      {
+        name = "path",
+        keyword_length = 3,
+      },
     }, {
       {
-        name = 'buffer',
-        keyword_length = 3
+        name = "buffer",
       },
     }),
     formatting = {
-      format = lspkind.cmp_format({ with_text = false, maxwidth = 50 })
-    }
-  })
+      format = lspkind.cmp_format { with_text = false, maxwidth = 50 },
+    },
+  }
 
   -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline({ '/', '?' }, {
+  cmp.setup.cmdline({ "/", "?" }, {
     mapping = cmp.mapping.preset.cmdline(),
     sources = {
-      { name = 'buffer' }
-    }
+      { name = "buffer" },
+    },
   })
 
   -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline(':', {
+  cmp.setup.cmdline(":", {
     mapping = cmp.mapping.preset.cmdline(),
     sources = cmp.config.sources({
-      { name = 'path' }
+      { name = "path" },
     }, {
-      { name = 'cmdline',
+      {
+        name = "cmdline",
         option = {
-          ignore_cmds = { 'Man', '!', "\'<,\'>!" }
+          ignore_cmds = { "Man", "!", "'<,'>!" },
         },
-        keyword_length = 4 }
-    })
+        keyword_length = 4,
+      },
+    }),
   })
 
   vim.cmd [[
--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -2,6 +2,25 @@ local M = {}
 
 M.config = function()
   local null_ls = require "null-ls"
+  require("mason-null-ls").setup {
+    ensure_installed = {
+      "prettierd",
+      "markdownlint",
+      "eslint_d",
+      "fish",
+      "fish_indent",
+      "stylua",
+      "golangci_lint",
+    },
+    automatic_installation = false,
+    handlers = {
+      prettierd = function(source_name, method)
+        null_ls.register(null_ls.builtins.formatting.prettierd.with {
+          disabled_filetypes = { "markdown" },
+        })
+      end,
+    },
+  }
   local augroup = vim.api.nvim_create_augroup("FormatOnSave", {})
 
   local formatfunc = function(_, bufnr)
@@ -22,18 +41,20 @@ M.config = function()
 
   null_ls.setup {
     sources = {
-      null_ls.builtins.formatting.prettierd.with {
-        disabled_filetypes = { "markdown" },
-      },
-      null_ls.builtins.diagnostics.eslint_d.with {
-        diagnostics_format = "[eslint] #{m}\n(#{c})",
-      },
-      null_ls.builtins.formatting.eslint_d,
-      null_ls.builtins.code_actions.eslint_d,
-      null_ls.builtins.diagnostics.fish,
-      null_ls.builtins.formatting.fish_indent,
-      null_ls.builtins.diagnostics.golangci_lint,
-      null_ls.builtins.formatting.stylua,
+      -- null_ls.builtins.formatting.prettierd.with {
+      --   disabled_filetypes = { "markdown" },
+      -- },
+      -- null_ls.builtins.diagnostics.eslint_d.with {
+      --   diagnostics_format = "[eslint] #{m}\n(#{c})",
+      -- },
+      -- null_ls.builtins.diagnostics.markdownlint,
+      -- null_ls.builtins.formatting.markdownlint,
+      -- null_ls.builtins.formatting.eslint_d,
+      -- null_ls.builtins.code_actions.eslint_d,
+      -- null_ls.builtins.diagnostics.fish,
+      -- null_ls.builtins.formatting.fish_indent,
+      -- null_ls.builtins.diagnostics.golangci_lint,
+      -- null_ls.builtins.formatting.stylua,
       require "typescript.extensions.null-ls.code-actions",
     },
     on_attach = function(client, bufnr)
