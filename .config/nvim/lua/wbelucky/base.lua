c5f6259da4b836dc7b6f6bb5d78ae1617ffe87db
wbelucky <39439193+WBelucky@users.noreply.github.com>
2023-05-29 00:29:42 +0900

    add emoji

--- .config/nvim/lua/wbelucky/base.lua
+++ .config/nvim/lua/wbelucky/base.lua
@@ -1,16 +1,16 @@
-vim.cmd('autocmd!')
+vim.cmd "autocmd!"
 
 vim.g.mapleader = " "
 
-vim.scriptencoding = 'utf-8'
-vim.opt.encoding = 'utf-8'
-vim.opt.fileencoding = 'utf-8'
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
 
 -- Don't redraw while executing macros (good performance config)
 vim.opt.lazyredraw = true
 vim.opt.termguicolors = true
 
-vim.opt.shell = 'fish'
+vim.opt.shell = "fish"
 
 -- editor view
 vim.opt.number = true
@@ -27,7 +27,6 @@ vim.opt.swapfile = false
 vim.opt.autoread = true -- autoread if the file edited out of the vim.
 vim.opt.hidden = true -- can open another file if the buffer is not saved
 
-
 -- space by tab
 vim.opt.tabstop = 2
 vim.opt.expandtab = true --insert [softtabstop] spaces by tab key
@@ -36,11 +35,11 @@ vim.opt.softtabstop = -1 -- spaces to insert or delete by tab or delete, -1 mean
 -- replace special charactor
 vim.opt.list = true
 vim.opt.listchars = {
-  tab = '»-',
-  trail = '-',
-  extends = '»',
-  precedes = '«',
-  nbsp = '%',
+  tab = "»-",
+  trail = "-",
+  extends = "»",
+  precedes = "«",
+  nbsp = "%",
 }
 
 -- indent
@@ -54,7 +53,7 @@ vim.opt.ignorecase = true --大文字や小文字の区別なく検索
 vim.opt.smartcase = true --大文字が含まれていた場合は区別する
 vim.opt.hlsearch = true --add highlight to search result
 vim.opt.incsearch = true --search string before confirm
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = "split"
 
 -- status and command line
 vim.opt.laststatus = 2 -- print some status
@@ -63,41 +62,43 @@ vim.opt.wildmenu = true --print candidates of :command line
 vim.opt.infercase = true --補完時に大文字小文字を区別しない
 
 -- clipboards
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
 -- Suppress appending <PasteStart> and <PasteEnd> when pasting
 -- TODO: set t_BE=
 
 -- TODO: set nosc noru nosm
 
 -- Turn off paste mode when leaving insert
-vim.api.nvim_create_autocmd('InsertLeave', {
-  pattern = '*',
-  command = 'set nopaste'
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*",
+  command = "set nopaste",
 })
 
 -- https://github.com/neovim/neovim/wiki/FAQ#how-to-use-the-windows-clipboard-from-wsl
-if vim.fn.has('wsl') == 1 then
-  vim.g.netrw_browsex_viewer = 'cmd.exe /C start'
+if vim.fn.has [[wsl]] == 1 then
+  vim.g.netrw_browsex_viewer = "cmd.exe /C start"
   vim.g.clipboard = {
-    name = 'win32yank',
+    name = "win32yank",
     copy = {
-      ['+'] = 'win32yank.exe -i --crlf',
-      ['*'] = 'win32yank.exe -i --crlf',
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
     },
     paste = {
-      ['+'] = 'win32yank.exe -o --lf',
-      ['*'] = 'win32yank.exe -o --lf',
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
     },
     cache_enabled = 1,
   }
 end
+
+-- vim.opt.spell = true
+-- vim.opt.spelllang = { "en", "cjk" }
 
 -- filetypes
 -- TODO: au BufNewFile,BufRead *.fish setl filetype=fish
 -- TODO: au BufNewFile,BufRead *.tsx setl filetype=typescriptreact
 -- TODO: au BufNewFile,BufRead *.md setl filetype=markdown
 -- TODO: au BufNewFile,BufRead *.mdx setl filetype=markdown
-
 
 -- TODO: runtime ./maps.vim
 -- TODO: runtime ./plug.vim
