490671ef05ca235ab9a5d855c8d2a6d67528d174
wbelucky <39439193+WBelucky@users.noreply.github.com>
2023-06-04 20:12:17 +0900

    install cargo & deno in install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,13 @@
 
 # FIXME aqua.sh is called before link.sh which needs yq
 
-curl https://sh.rustup.rs -sSf | sh
+. ./scripts/cargo.sh
 . ./scripts/aqua.sh
 
 . ./scripts/link.sh
 
-. ./scripts/vim-plug.sh
+. ./scripts/deno.sh
+. ./scripts/packer.sh
 . ./scripts/asdf.sh
 . ./scripts/fisher.sh
 
