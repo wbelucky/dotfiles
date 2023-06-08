1684d81d10c94bb52a511ae58c02efd2937e53c2
wbelucky <39439193+WBelucky@users.noreply.github.com>
2023-06-08 13:47:43 +0900

    fix arm -> aarch

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -35,7 +35,7 @@ set -gx AQUA_ROOT_DIR $XDG_DATA_HOME/aquaproj-aqua
 fish_add_path $AQUA_ROOT_DIR/bin
 
 set -gx AQUA_GLOBAL_CONFIG $XDG_CONFIG_HOME/aquaproj-aqua/aqua.yaml
-if uname -m | not grep -q arm
+if uname -m | not grep -q aarch
     set -gx AQUA_GLOBAL_CONFIG "$XDG_CONFIG_HOME/aquaproj-aqua/neovim-x86_64.yaml:$AQUA_GLOBAL_CONFIG"
 end
 
