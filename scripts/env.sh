f7232270e4bde7cf9d6c030d4f90bb19b2c6b40e
wbelucky <39439193+WBelucky@users.noreply.github.com>
2023-07-25 17:55:21 +0900

    make AQUA_GLOBAL_CONFIG in scripts/env.sh consider architecture

--- scripts/env.sh
+++ scripts/env.sh
@@ -6,6 +6,10 @@ DOTFILES=$(pwd)
 PATH="$HOME/.cargo/bin:$PATH"
 PATH="$HOME/.local/share/aquaproj-aqua/bin:$PATH"
 export AQUA_GLOBAL_CONFIG="$DOTFILES/.config/aquaproj-aqua/aqua.yaml"
+
+if [ "$(uname -m)" != "aarch" ]; then
+    export AQUA_GLOBAL_CONFIG="$DOTFILES/.config/aquaproj-aqua/amd64.yaml:$AQUA_GLOBAL_CONFIG"
+fi
 export AQUA_POLICY_CONFIG="$DOTFILES/.config/aquaproj-aqua/aqua-policy.yaml"
 
 popd
