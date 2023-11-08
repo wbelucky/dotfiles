3292a4ae6a9503eb3d521bedcaa512a8897647ab
wbelucky <39439193+WBelucky@users.noreply.github.com>
2023-11-08 14:42:49 +0900

    fix md-link-title.vim spec

---
+++ .config/nvim/lua/rc/plugins/md-link-title.lua
@@ -0,0 +1,9 @@
+---@type LazySpec
+local  spec =  {
+    "wbelucky/md-link-title.vim",
+    dev = true,
+    ft = "markdown",
+    dependencies = { "vim-denops/denops.vim" },
+  }
+
+return spec
--- .config/nvim/lua/rc/plugins/misc.lua
+++ .config/nvim/lua/rc/plugins/misc.lua
@@ -2,14 +2,13 @@
 local spec = {
   {
     "windwp/nvim-ts-autotag",
+    enabled = false,
     event = "VeryLazy",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
   },
   { "tpope/vim-surround", event = "VeryLazy" },
-  { "vim-denops/denops.vim" },
   { "buoto/gotests-vim", ft = "go" },
   { "dstein64/vim-startuptime", event = "VeryLazy" },
-  { "wbelucky/md-link-title.vim", dev = true },
 }
 
 return spec
