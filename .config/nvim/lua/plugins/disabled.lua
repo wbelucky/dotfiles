5a5eaaef8b440e89e08460bbcd769f780415eac2
wbelucky <39439193+WBelucky@users.noreply.github.com>
2024-04-21 01:08:31 +0900

    add disabled.lua

---
+++ .config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,7 @@
+--
+return {
+  { "folke/flash.nvim", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "echasnovski/mini.pairs", enabled = false },
+  { "nvimdev/dashboard-nvim", enabled = false },
+}
