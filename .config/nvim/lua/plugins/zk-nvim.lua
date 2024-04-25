83b74a4c17144e4a0f179d0d711c8e619b26ac8a
wbelucky <39439193+WBelucky@users.noreply.github.com>
2024-04-25 17:09:55 +0900

    add ZkNotes on Sprint (tags = todo or in-progress)

--- .config/nvim/lua/plugins/zk-nvim.lua
+++ .config/nvim/lua/plugins/zk-nvim.lua
@@ -1,3 +1,48 @@
+---@param options? table additional options
+---@param picker_options? table options for the picker
+---@param cb function
+local function pickTodoOrInProgress(options, picker_options, cb)
+  local ui = require "zk.ui"
+  local api = require "zk.api"
+  ---@see zk.ui.pick_notes
+  options =
+    vim.tbl_extend("force", { select = ui.get_pick_notes_list_api_selection(picker_options or {}) }, options or {})
+
+  -- FIXME: use coroutine
+  api.list(options.notebook_path, vim.tbl_extend("force", options, { tags = { "todo" } }), function(errToDo, notesToDo)
+    assert(not errToDo, tostring(errToDo))
+
+    api.list(
+      options.notebook_path,
+      vim.tbl_extend("force", options, { tags = { "in-progress" } }),
+      function(errInProgress, notesInProgress)
+        assert(not errInProgress, tostring(errInProgress))
+        ui.pick_notes(vim.list_extend(notesInProgress, notesToDo), picker_options, cb)
+      end
+    )
+  end)
+end
+
+---@param options? table additional options
+---@param picker_options? table options for the picker
+---@see https://github.com/zk-org/zk/blob/main/docs/editors-integration.md#zklist
+---@see zk.ui.pick_notes
+local function editTodoOrInProgress(options, picker_options)
+  local zk = require "zk"
+  local config = require "zk.config"
+  pickTodoOrInProgress(options, picker_options, function(notes)
+    if picker_options and picker_options.multi_select == false then
+      notes = { notes }
+    end
+    if config.options.cd_on_edit then
+      zk.cd()
+    end
+    for _, note in ipairs(notes) do
+      vim.cmd("e " .. note.absPath)
+    end
+  end)
+end
+
 ---@type LazySpec
 local spec = {
   "wbelucky/zk-nvim",
@@ -44,6 +89,14 @@ local spec = {
         require("zk.commands").get "ZkTags" {}
       end,
       desc = "ZkTags",
+      mode = "n",
+    },
+    {
+      "<leader>ms",
+      function()
+        editTodoOrInProgress({}, { title = "Zk on Sprint" })
+      end,
+      desc = "Pick notes on [S]print",
       mode = "n",
     },
   },
