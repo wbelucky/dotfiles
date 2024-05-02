0657c2b9bbb366c55ebc7b453a52962754c7a7e4
wbelucky <39439193+WBelucky@users.noreply.github.com>
2024-05-02 20:43:54 +0900

    fix: move InsertLeave autocmd in md-link-title spec to plugin

--- .config/nvim/lua/plugins/md-link-title.lua
+++ .config/nvim/lua/plugins/md-link-title.lua
@@ -8,25 +8,6 @@ local spec = {
     if spec.dev then
       vim.g["denops#debug"] = 1
     end
-
-    -- vim.cmd [[command! -nargs=0 -range Link <line1>,<line2>call md_link_title#replace()]]
-    vim.api.nvim_create_user_command("Link", [[<line1>,<line2>MdLinkTitle]], { nargs = 0, range = true })
-
-    local augroup = vim.api.nvim_create_augroup("MdLinkTitle", { clear = true })
-
-    vim.api.nvim_create_autocmd("InsertLeave", {
-      group = augroup,
-      pattern = "*.md",
-      callback = function()
-        local firstline = vim.fn.line "'["
-        local lastline = vim.fn.line "']"
-        vim.fn["denops#notify"]("md-link-title", "replace", { firstline, lastline })
-      end,
-    })
-
-    vim.api.nvim_create_user_command("MdLinkTitleDisableAutoModify", function()
-      vim.api.nvim_clear_autocmds { group = augroup }
-    end, { nargs = 0 })
   end,
   dependencies = { "vim-denops/denops.vim" },
 }
